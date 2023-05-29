909e21afe299d1afa7d91c07901e15b148365df3
TUGU617 <132142382+TUGU617@users.noreply.github.com>
2023-05-29 17:34:28 +0900

    ユーザー管理機能

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ApplicationRecord
+  validates :nickname, presence: true
+  validates :last_name, presence: true,format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ }
+  validates :first_name, presence: true,format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ }
+  validates :last_name_kana, presence: true,format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, presence: true,format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :date_of_birth, presence: true
+  validate :password_complexity
+
+  private
+
+  def password_complexity
+    if password.present? && !password.match(/\A(?=.*?[a-zA-Z])(?=.*?[0-9])/)
+      errors.add(:password, 'は半角英字と半角数字の両方を含める必要があります')
+    end
+  end
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials}
+    email                 {Faker::Internet.free_email }
+    password              {Faker::Internet.password(min_length: 6) }
+    password_confirmation {password}
+    last_name             {"あア阿"}
+    first_name            {"いイ胃"}
+    last_name_kana        {"カタカナ"}
+    first_name_kana       {"カタカナ"}
+    date_of_birth         {Faker::Date.birthday(min_age: 18, max_age: 65)}
+    
+  end
+end
