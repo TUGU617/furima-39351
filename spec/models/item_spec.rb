f539e9fb188d88b8c78fb027798bf73f84dd5c11
TUGU617 <132142382+TUGU617@users.noreply.github.com>
2023-06-01 17:09:38 +0900

    商品出品機能2

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -17,41 +17,74 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Image can't be blank")
       end
+
       it "商品名が必須であること" do
         @item.product_name = ""
         @item.valid?
         expect(@item.errors.full_messages).to include("Product name can't be blank")
       end
+
       it "商品の説明が必須であること" do
         @item.explanation = ""
         @item.valid?
         expect(@item.errors.full_messages).to include("Explanation can't be blank")
       end
+
       it "カテゴリーの情報が必須であること" do
         @item.category_id = ""
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
+      it "カテゴリーに「---」が選択されている場合は出品できない" do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+
       it "商品の状態の情報が必須であること" do
         @item.situation_id = ""
         @item.valid?
         expect(@item.errors.full_messages).to include("Situation can't be blank")
       end
+      it "商品の状態に「---」が選択されている場合は出品できない" do
+        @item.situation_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Situation can't be blank")
+      end
+
       it "配送料の負担の情報が必須であること" do
         @item.delivery_charge_id = ""
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery charge can't be blank")
       end
+      it "配送料の負担に「---」が選択されている場合は出品できない" do
+        @item.delivery_charge_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery charge can't be blank")
+      end
+
       it "発送元の地域の情報が必須であること" do
         @item.prefecture_id = ""
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
+      it "発送元の地域に「---」が選択されている場合は出品できない" do
+        @item.prefecture_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+
       it "発送までの日数の情報が必須であること" do
         @item.date_of_shipment_id = ""
         @item.valid?
         expect(@item.errors.full_messages).to include("Date of shipment can't be blank")
       end
+      it "発送までの日数に「---」が選択されている場合は出品できない" do
+        @item.date_of_shipment_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Date of shipment can't be blank")
+      end
+
       it "価格の情報が必須であること" do
         @item.price = ""
         @item.valid?
@@ -71,6 +104,12 @@ RSpec.describe Item, type: :model do
         @item.price = "５００"
         @item.valid?
         expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+
+      it "userが紐付いていなければ出品できない" do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
       end
     end
   end
