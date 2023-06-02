e4647ac96b005552760fc6f3573781f43e337b04
TUGU617 <132142382+TUGU617@users.noreply.github.com>
2023-06-02 22:45:56 +0900

    商品情報編集機能8
    ・ログインの有無で遷移するページに
    　制限をかけた

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,4 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit]
 
   def index
@@ -24,6 +23,11 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    if user_signed_in? && current_user == @item.user
+      render :edit
+    else
+      redirect_to root_path
+    end
   end
 
   def update
