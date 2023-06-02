0310f6cf3be9dcc393721e818f27c2ddd5359ff5
TUGU617 <132142382+TUGU617@users.noreply.github.com>
2023-06-02 18:10:45 +0900

    商品情報編集機能
    ・編集機能のためeditとpudate
    　アクションをルーティングと
    　コントローラーに記述した

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,6 +21,18 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
+  end
   private
   def item_params
     params.require(:item).permit(:product_name, :explanation, :category_id, :situation_id, :delivery_charge_id, :prefecture_id, :date_of_shipment_id, :price, :image).merge(user_id: current_user.id)
